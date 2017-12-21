b0d6eed331bcfed5abf1a0a1a385b24b4712d57f
lizhonghuan <lizhonghuan@100tal.com>
2017-12-21 18:25:52 +0800

    RainCat游戏基础架构

--- SpiritKit/SpiritKit/GameScene.swift
+++ SpiritKit/SpiritKit/GameScene.swift
@@ -14,6 +14,7 @@ class GameScene: SKScene {
     var entities = [GKEntity]()
     var graphs = [String : GKGraph]()
     
+    
     private var lastUpdateTime : TimeInterval = 0
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
